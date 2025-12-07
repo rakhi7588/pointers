605b065209f257e3a0b339349d5db4ddfa04d7eb
rakhi7588 <rakhijha918@gmail.com>
2025-12-07 17:30:06 +0100

    second  commit

--- array.c
+++ array.c
@@ -1,16 +1,17 @@
 #include <stdio.h>
 int main(){
-int i;
+int i; int sum=0;
 int a[10];
 printf("Enter 10 numbers\n");
 
-for (i=0; i<10; i++) {
+for (i=0; i<10; i++) 
   scanf("%d", &a[i]);
+for (i=0; i<10; i++) { 
+  printf("a[%d]=%d\n",i, a[i]); 
+  sum = sum + a[i];
 
-  printf("a[%d]=%d\n",i, a[i]); }
-
-
-
+ }
+printf("sum:%d\n",sum);
 
 
 return 0;    
