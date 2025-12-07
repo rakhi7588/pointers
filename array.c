d68454fe856dd9a3e04ad082b2d8de59bf2ef81a
rakhi7588 <rakhijha918@gmail.com>
2025-12-07 17:14:03 +0100

    first commit

---
+++ array.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main(){
+int i;
+int a[10];
+printf("Enter 10 numbers\n");
+
+for (i=0; i<10; i++) {
+  scanf("%d", &a[i]);
+
+  printf("a[%d]=%d\n",i, a[i]); }
+
+
+
+
+
+return 0;    
+}
